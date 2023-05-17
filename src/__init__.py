69f6caa40e955c07a7440323cff5a080a0cab381
anivanchen <ivanchen07@gmail.com>
2023-05-17 18:51:29 -0400

    Add components to init.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,19 +1,34 @@
 import cv2
 from pipeline.Capture import DefaultCapture
 from pipeline.Detector import FiducialDetector
+from pipeline.PoseEstimator import FiducialPoseEstimator
 from config.Config import Config, LocalConfig, RemoteConfig
 from config.ConfigManager import FileConfigManager, NTConfigManager
+from output.Annotate import AnnotateFiducials
 
 config = Config(LocalConfig(), RemoteConfig())
 file_config_manager = FileConfigManager()
 nt_config_manager = NTConfigManager()
 
-capture = DefaultCapture()
-detector = FiducialDetector(config)
-
 file_config_manager.update(config)
 nt_config_manager.update(config)
 
+capture = DefaultCapture()
+detector = FiducialDetector(config)
+pose_estimator = FiducialPoseEstimator(config)
+annotator = AnnotateFiducials(config)
+
 while True:
-    capture.getFrame(config)
-    cv2.imshow("Capture", capture.frame)
+    frame = capture.getFrame(config)
+    corners, ids = detector.detect(frame)
+    frame = cv2.aruco.drawDetectedMarkers(frame, corners, ids)
+    rvecs, tvecs = pose_estimator.process(corners, ids)
+    frame = annotator.annotate(frame, rvecs, tvecs, config)
+
+    cv2.imshow("Capture", frame)
+
+    if cv2.waitKey(1) == 27:
+        break
+
+capture.release()
+cv2.destroyAllWindows()
