b0bda5c539b9cd4c91361718a56419d654b78b76
anivanchen <ivanchen07@gmail.com>
2023-05-17 18:50:10 -0400

    update object_points to reference self

--- src/pipeline/PoseEstimator.py
+++ src/pipeline/PoseEstimator.py
@@ -15,25 +15,25 @@ class FiducialPoseEstimator(PoseEstimator):
     fiducial_size = 0.0
     object_points = numpy.array([])
     camera_matrix = None
-    distortion_coefficients = None
+    distortion_coefficient = None
 
     def __init__(self, config: Config):
         self.fiducial_size = config.local.fiducial_size
-        self.object_points = np.asarray([
-                [-fiducial_size / 2, fiducial_size / 2, 0],
-                [fiducial_size / 2, fiducial_size / 2, 0],
-                [fiducial_size / 2, -fiducial_size / 2, 0],
-                [-fiducial_size / 2, -fiducial_size / 2, 0]
+        self.object_points = numpy.asarray([
+                [-self.fiducial_size / 2, self.fiducial_size / 2, 0],
+                [self.fiducial_size / 2, self.fiducial_size / 2, 0],
+                [self.fiducial_size / 2, -self.fiducial_size / 2, 0],
+                [-self.fiducial_size / 2, -self.fiducial_size / 2, 0]
             ], dtype=numpy.float32)
         self.camera_matrix = config.local.camera_matrix
-        self.distortion_coefficients = config.local.distortion_coefficients
+        self.distortion_coefficient = config.local.distortion_coefficient
     
     def process(self, corners, ids):
         rvecs = []
         tvecs = []
 
         for i, id in enumerate(ids):
-            retval, rvec, tvec = cv2.solvePnP(object_points, corners[i], camera_matrix, distortion_coefficients)
+            retval, rvec, tvec = cv2.solvePnP(self.object_points, corners[i], self.camera_matrix, self.distortion_coefficient, flags=cv2.SOLVEPNP_SQPNP)
             rvecs.append(rvec)
             tvecs.append(tvec)
 
