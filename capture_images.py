3bd42bd2fdbde04d886ecdf9353e2b16730ae9cf
Ivan Chen <ivanchen07@gmail.com>
2023-05-16 21:44:47 -0400

    Update and test manual capture and calibrate scripts

--- capture_images.py
+++ capture_images.py
@@ -1,6 +1,7 @@
 # Captures camera stream and writes image on user request
 
 import cv2
+import os
 import time
 
 capture = cv2.VideoCapture(0)
@@ -11,18 +12,20 @@ images = 0
 
 while True:
 
-    image = capture.read()
+    _, image = capture.read()
 
     charucoCorners, charucoIDs, markerCorners, markerIDs = charucoDetector.detectBoard(image)
-    image = cv2.drawDetectedCornersCharuco(image, charucoCorners, charucoIDs)
-    cv2.putText(image, "Captured Images: " + str(i), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+
     cv2.imshow("Capture", image)
     
-    if charucoCorners is not None and charucoIDs is not None and cv2.waitKey() == 32: # space
-        cv2.imwrite("/captures/" + time.now() + "_capture.png", image)
+    if charucoCorners is not None and charucoIDs is not None and cv2.waitKey(1) == 32: # space
+        cv2.imwrite(os.getcwd() + "/captures/" + str(round(time.time())) + "_capture.png", image)
         images += 1
+        print("Images Captured: " + str(images))
+    else:
+        pass
 
-    if cv2.waitKey() == 27: # esc
+    if cv2.waitKey(1) == 27: # esc
         break
 
 capture.release()
--- manual_calibration.py
+++ manual_calibration.py
@@ -2,17 +2,15 @@
 # Outputs camera matrix and distortion coefficients to json file and command line
 
 import cv2
-import numpy
 import os
+import numpy
 import time
 import json
 import datetime
 
-images_path = "/captures"
+images_path = os.getcwd() + "/captures"
 output_file_name = "calibration.json"
 dictionary = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_5X5_100)
-parameters = cv2.aruco.DetectorParameters()
-arucoDetector = cv2.aruco.ArucoDetector(dictionary, parameters)
 board = cv2.aruco.CharucoBoard((12,9), 0.03, 0.023, dictionary)
 charucoDetector = cv2.aruco.CharucoDetector(board)
 
@@ -24,10 +22,10 @@ i = 0
 
 print("Starting calibration using images in " + images_path)
 
-for image in os.listdir(os.cwd() + images_path):
-    image = cv2.imread(image)
-    markers, ids, rejected = arucoDetector.detectMarkers(image)
-    charucoCorners, charucoIDs, markerCorners, markerIDs = charucoDetector.detectBoard(image, corners, ids)
+for image in os.listdir(images_path):
+    image = cv2.imread(images_path + "/" + image)
+    cv2.imshow("Image", image)
+    charucoCorners, charucoIDs, markerCorners, markerIDs = charucoDetector.detectBoard(image)
     
     if charucoCorners is not None and charucoIDs is not None:
         allCorners.append(charucoCorners)
@@ -38,17 +36,17 @@ for image in os.listdir(os.cwd() + images_path):
 
 retval, camera_matrix, distortion_coefficient, rvecs, tvecs = cv2.aruco.calibrateCameraCharuco(allCorners, allIDs, board, imsize, None, None)
 
-print("Calibration complete for " str(i) + " images in " + str(time.time() - time_start) + " seconds")
+print("Calibration complete for " + str(i) + " images in " + str(time.time() - time_start) + " seconds")
 print("Camera Matrix")
 print(camera_matrix)
 print("Distortion Coefficients")
 print(distortion_coefficient)
 
 output = {
-    "number_of_images": i
-    "timestamp": datetime.datetime.now()
-    "camera_matrix": camera_matrix,
-    "distortion_coefficient": distortion_coefficient
+    "number_of_images": i,
+    "timestamp": str(datetime.datetime.now()),
+    "camera_matrix": numpy.ndarray.tolist(camera_matrix),
+    "distortion_coefficient": numpy.ndarray.tolist(distortion_coefficient)
 }
 
 with open(output_file_name, "w") as file:
