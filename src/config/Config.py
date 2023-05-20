9d62d07d75041f8b57c8353254b164a383cf5d3d
anivanchen <ivanchen07@gmail.com>
2023-05-20 10:06:15 -0400

    Update config files

--- src/config/Config.py
+++ src/config/Config.py
@@ -6,7 +6,7 @@ from dataclasses import dataclass, field
 class LocalConfig:
     device_id: str = ""
     server_ip: str = ""
-    stream_port: int = 1000
+    stream_port: int = 5802
     calibrated: bool = False
     fiducial_size: float = 0
     detection_dictionary: any = None
@@ -20,10 +20,10 @@ class LocalConfig:
 @dataclass
 class RemoteConfig:
     camera_id: int = 0
-    camera_resolution_width: int = 0
-    camera_resolution_height: int = 0
-    camera_auto_exposure: int = 0
-    camera_exposure: int = 0
+    camera_resolution_width: int = 1600
+    camera_resolution_height: int = 1200
+    camera_auto_exposure: int = -1
+    camera_exposure: int = 1000
     camera_gain: int = 0
     fiducial_layout: any = field(default_factory=list)
 
