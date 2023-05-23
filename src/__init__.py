d5e048842f393ebd62741d06165e4ac5ce49bbcf
anivanchen <ivanchen07@gmail.com>
2023-05-23 10:32:43 -0400

    Remove matplotlib and add publisher

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,5 @@
 import time
 import cv2
-import matplotlib.pyplot as plt
 
 from pipeline.Capture import DefaultCapture
 from pipeline.Detector import FiducialDetector
@@ -9,6 +8,7 @@ from config.Config import Config, LocalConfig, RemoteConfig
 from config.ConfigManager import FileConfigManager, NTConfigManager
 from output.Annotate import AnnotateFiducials
 from output.Stream import MJPGServer
+from output.Publisher import NTPublisher
 
 config = Config(LocalConfig(), RemoteConfig())
 file_config_manager = FileConfigManager()
@@ -23,6 +23,7 @@ pose_estimator = FiducialPoseEstimator(config)
 camera_pose_estimator = CameraPoseEstimator()
 annotator = AnnotateFiducials(config)
 stream = MJPGServer()
+publisher = NTPublisher()
 
 stream.start(config)
 
@@ -46,40 +47,6 @@ while True:
         counter = 0
     pt = time.time() - pt_start_time
 
-    if pose is not None:
-        allPoses.append(pose)
     frame = annotator.annotate(frame, rvecs, tvecs, fps, pt, config)
+    publisher.send(config, time.time(), fps, pose)
     stream.set_frame(frame)
-
-    # cv2.imshow("Capture", frame)
-
-    if cv2.waitKey(1) == 27:
-        break
-
-capture.release()
-cv2.destroyAllWindows()
-
-x = []
-y = []
-z = []
-
-print(allPoses)
-
-for pose in allPoses:
-        x.append(pose[0][0])
-        y.append(pose[0][1])
-        z.append(pose[0][2])
-
-fig = plt.figure()
-ax = fig.add_subplot(projection='3d')
-
-ax.axes.set_xlim3d(0, 1) 
-ax.axes.set_ylim3d(0, 1) 
-ax.axes.set_zlim3d(0, 1) 
-
-ax.set_xlabel('X')
-ax.set_ylabel('Z')
-ax.set_zlabel('Y')
-
-ax.plot(x, y, z)
-plt.show()
