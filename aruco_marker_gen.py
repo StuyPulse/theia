4bad3b9b368a2d4ea633ffdf2d09e679780dc05c
Ivan Chen <ivanchen07@gmail.com>
2023-09-22 10:05:07 -0400

    Fix bugs & Add user input for marker width
    Add check for output directory
    Fix image not reinitialized bug
    Fix offset_text calculation bug
    Fix A4 paper size
    Add user input for marker width (in)

--- aruco_marker_gen.py
+++ aruco_marker_gen.py
@@ -1,61 +1,73 @@
-import cv2, time, numpy
+import os, cv2, numpy
 
 from PIL import Image, ImageDraw, ImageFont
 
-
 FAMILY_DICT = {
-    "4X4_50": cv2.aruco.DICT_4X4_50,
-    "4X4_100": cv2.aruco.DICT_4X4_100,
-    "4X4_250": cv2.aruco.DICT_4X4_250,
-    "4X4_1000": cv2.aruco.DICT_4X4_1000,
-    "5X5_50": cv2.aruco.DICT_5X5_50,
-    "5X5_100": cv2.aruco.DICT_5X5_100,
-    "5X5_250": cv2.aruco.DICT_5X5_250,
-    "5X5_1000": cv2.aruco.DICT_5X5_1000,
-    "6X6_50": cv2.aruco.DICT_6X6_50,
-    "6X6_100": cv2.aruco.DICT_6X6_100,
-    "6X6_250": cv2.aruco.DICT_6X6_250,
-    "6X6_1000": cv2.aruco.DICT_6X6_1000,
-    "7X7_50": cv2.aruco.DICT_7X7_50,
-    "7X7_100": cv2.aruco.DICT_7X7_100,
-    "7X7_250": cv2.aruco.DICT_7X7_250,
-    "7X7_1000": cv2.aruco.DICT_7X7_1000,
-    "ARUCO_ORIGINAL": cv2.aruco.DICT_ARUCO_ORIGINAL,
-    "APRILTAG_16H5": cv2.aruco.DICT_APRILTAG_16h5,
-    "APRILTAG_25H9": cv2.aruco.DICT_APRILTAG_25h9,
-    "APRILTAG_36H10": cv2.aruco.DICT_APRILTAG_36h10,
-    "APRILTAG_36H11": cv2.aruco.DICT_APRILTAG_36h11
+    '4X4_50': cv2.aruco.DICT_4X4_50,
+    '4X4_100': cv2.aruco.DICT_4X4_100,
+    '4X4_250': cv2.aruco.DICT_4X4_250,
+    '4X4_1000': cv2.aruco.DICT_4X4_1000,
+    '5X5_50': cv2.aruco.DICT_5X5_50,
+    '5X5_100': cv2.aruco.DICT_5X5_100,
+    '5X5_250': cv2.aruco.DICT_5X5_250,
+    '5X5_1000': cv2.aruco.DICT_5X5_1000,
+    '6X6_50': cv2.aruco.DICT_6X6_50,
+    '6X6_100': cv2.aruco.DICT_6X6_100,
+    '6X6_250': cv2.aruco.DICT_6X6_250,
+    '6X6_1000': cv2.aruco.DICT_6X6_1000,
+    '7X7_50': cv2.aruco.DICT_7X7_50,
+    '7X7_100': cv2.aruco.DICT_7X7_100,
+    '7X7_250': cv2.aruco.DICT_7X7_250,
+    '7X7_1000': cv2.aruco.DICT_7X7_1000,
+    'ARUCO_ORIGINAL': cv2.aruco.DICT_ARUCO_ORIGINAL,
+    'APRILTAG_16H5': cv2.aruco.DICT_APRILTAG_16h5,
+    'APRILTAG_25H9': cv2.aruco.DICT_APRILTAG_25h9,
+    'APRILTAG_36H10': cv2.aruco.DICT_APRILTAG_36h10,
+    'APRILTAG_36H11': cv2.aruco.DICT_APRILTAG_36h11
 }
 
-print("Available dictionaries:")
+print('Available dictionaries:')
 for key in FAMILY_DICT.keys():
     print(key)
 print()
 
-dictionary_choice = "APRILTAG_36H10"
+dictionary_choice = ''
 while dictionary_choice not in FAMILY_DICT.keys():
-    dictionary_choice = input("Choose the dictionary: ").upper()
+    dictionary_choice = input('Choose the dictionary: ').upper()
 dictionary = cv2.aruco.getPredefinedDictionary(FAMILY_DICT[dictionary_choice])
 
 dpi = 300
-a4_size = (int(numpy.around((210 * dpi)/25.4)) , int(numpy.around((297 * dpi)/25.4)))
+a4_size = (int(numpy.around((210.058 * dpi) / 25.4)), int(numpy.around((296.93 * dpi) / 25.4)))
 
-image = Image.new('RGB',
+marker_in = float(input('Marker size (in): '))
+marker_width_pxl = int(marker_in * dpi)
+
+number = int(input('Number of markers: '))
+
+for i in range(number):
+
+    if not os.path.exists('marker_output/'):
+        os.mkdir('marker_output/')
+    elif os.path.exists('marker_output/' + dictionary_choice + '_' + str(i) + '.pdf'):
+        os.remove('marker_output/' + dictionary_choice + '_' + str(i) + '.pdf')
+
+    print('Generating ' + dictionary_choice + '_' + str(i) + '.pdf')
+
+    image = Image.new('RGB',
                  (a4_size[0],a4_size[1]),   # A4 at 300dpi
                  (255, 255, 255))
 
-marker_width = int(76.2 * dpi / 25.4)
-number = int(input("Number of Markers: "))
-for i in range(number):
-    marker = Image.fromarray(cv2.aruco.generateImageMarker(dictionary, i, marker_width))
-    offset_tag = ((image.size[0] - marker_width) // 2, (image.size[1] - marker_width) // 2)
+    marker = Image.fromarray(cv2.aruco.generateImageMarker(dictionary, i, marker_width_pxl))
+    offset_tag = ((image.size[0] - marker_width_pxl) // 2, (image.size[1] - marker_width_pxl) // 2)
     image.paste(marker, offset_tag)
 
     draw = ImageDraw.Draw(image)
-    fnt = ImageFont.truetype('Arial', 100)
-    text = "ID: " + str(i) + "\nFamily: " + dictionary_choice + "\nSize: " + str(marker_width) + " mm"
-    offset_text = (marker_width/4, ((image.size[1] - marker_width) / 2) + image.size[1] + 100 )
-    draw.text(offset_text, text, (0,0,0),font=fnt)
+    fnt = ImageFont.truetype('Arial', 56)
+    text = 'ID: ' + str(i) + '\nFamily: ' + dictionary_choice + '\nSize: ' + str(marker_in) + ' in'
+    offset_text = ((image.size[0] - marker_width_pxl) / 2, ((image.size[1] - marker_width_pxl) / 2) + marker_width_pxl + 192)
+    draw.text(offset_text, text.upper(), (0,0,0), font=fnt)
 
-    outputfile = "marker_output/" + dictionary_choice + "_" + str(i) + ".pdf"
+    outputfile = 'marker_output/' + dictionary_choice + '_' + str(i) + '.pdf'
     image.save(outputfile, 'PDF', quality=100)
+
+print('Done!')
