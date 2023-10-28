44167bfe0b9a0d58d7b6e4b585faae2cc1519de3
Ivan Chen <ivanchen07@gmail.com>
2023-10-28 18:05:59 -0400

    Update NT config manager
    Update config values from NT, properly parse fiducial_layout

--- src/config/ConfigManager.py
+++ src/config/ConfigManager.py
@@ -45,8 +45,6 @@ class FileConfigManager:
         config.local.team_number = config_data["team_number"]
         config.local.stream_port = config_data["stream_port"]
 
-        # config.local.calibrated = config_data["calibrated"]
-        config.local.fiducial_size = config_data["fiducial_size"]
         config.local.detection_dictionary = cv2.aruco.getPredefinedDictionary(FAMILY_DICTIONARY[config_data["detection_dictionary"]])
         config.local.calibration_dictionary = cv2.aruco.getPredefinedDictionary(FAMILY_DICTIONARY[config_data["calibration_dictionary"]])
 
@@ -61,7 +59,22 @@ class FileConfigManager:
         # Aruco Parameters
         config.local.aruco_parameters.cornerRefinementMethod = cv2.aruco.CORNER_REFINE_SUBPIX
         config.local.aruco_parameters.useAruco3Detection = True
-        config.local.aruco_parameters.cornerRefinementMaxIterations = 20
+        # config.local.aruco_parameters.adaptiveThreshWinSizeMin = 141  # default 3
+        # config.local.aruco_parameters.adaptiveThreshWinSizeMax = 251  # default 23
+        # config.local.aruco_parameters.adaptiveThreshWinSizeStep = 20  # default 10
+        # config.local.aruco_parameters.adaptiveThreshConstant = 4      # default 7
+        # config.local.aruco_parameters.adaptiveThreshWinSizeMin = 13    # default 3
+        # config.local.aruco_parameters.adaptiveThreshWinSizeMax = 53   # default 23
+        # config.local.aruco_parameters.adaptiveThreshWinSizeStep = 20  # default 10
+        # config.local.aruco_parameters.adaptiveThreshConstant = 7      # default 7
+        # config.local.aruco_parameters.cornerRefinementWinSize = 5
+        # config.local.aruco_parameters.cornerRefinementMaxIterations = 20
+        # config.local.aruco_parameters.cornerRefinementMinAccuracy = 0.1
+        # config.local.aruco_parameters.minMarkerPerimeterRate = 0.03
+        # config.local.aruco_parameters.maxMarkerPerimeterRate = 4.0
+        # config.local.aruco_parameters.polygonalApproxAccuracyRate = 0.05
+        # config.local.aruco_parameters.perspectiveRemovePixelPerCell = 4
+        # config.local.aruco_parameters.perspectiveRemoveIgnoredMarginPerCell = 0.13
 
         cbp = config_data["charuco_board"]
         config.local.charuco_board = cv2.aruco.CharucoBoard((int(cbp[0]), int(cbp[1])), cbp[2], cbp[3], config.local.calibration_dictionary)
@@ -85,14 +98,13 @@ class FileConfigManager:
 Configuration
 #############
 """)
-        print("Device ID: " + config.local.device_name)
+        print("Device Name: " + config.local.device_name)
         print("Server IP: " + config.local.server_ip)
         print("Stream Port: " + str(config.local.stream_port))
-        print("Fiducial Size: " + str(config.local.fiducial_size) + " m")
         print("Detection Dictionary: " + str(config_data["detection_dictionary"]))
         print("Calibration Dictionary: " + str(config_data["calibration_dictionary"]))
-        print("Camera Matrix: " + str(config.local.camera_matrix))
-        print("Distortion Coefficients: " + str(config.local.distortion_coefficient) + "\n")
+        print("Camera Matrix: \n" + str(config.local.camera_matrix))
+        print("Distortion Coefficients: \n" + str(config.local.distortion_coefficient) + "\n")
 
 class NTConfigManager:
     init_complete: bool = False
@@ -103,11 +115,18 @@ class NTConfigManager:
     camera_auto_exposure_sub: IntegerSubscriber
     camera_exposure_sub: IntegerSubscriber
     camera_gain_sub: DoubleSubscriber
+    camera_brightness_sub: DoubleSubscriber
+    fiducial_size_sub: DoubleSubscriber
     fiducial_layout_sub: DoubleArraySubscriber
+
+    def __init__(self):
+        pass
 
     def update(self, config: Config) -> None:
         if not self.init_complete:
-            table = NetworkTableInstance.getDefault().getTable("/" + config.local.device_name + "/config")
+            table = NetworkTableInstance.getDefault().getTable(config.local.device_name)
+            table = table.getSubTable("config")
+
             self.camera_id_sub = table.getIntegerTopic("camera_id").subscribe(RemoteConfig.camera_id)
             self.camera_resolution_width_sub = table.getIntegerTopic("camera_resolution_width").subscribe(RemoteConfig.camera_resolution_width)
             self.camera_resolution_height_sub = table.getIntegerTopic("camera_resolution_height").subscribe(RemoteConfig.camera_resolution_height)
@@ -115,19 +134,30 @@ class NTConfigManager:
             self.camera_exposure_sub = table.getIntegerTopic("camera_exposure").subscribe(RemoteConfig.camera_exposure)
             self.camera_gain_sub = table.getDoubleTopic("camera_gain").subscribe(RemoteConfig.camera_gain)
             self.camera_brightness_sub = table.getDoubleTopic("camera_brightness").subscribe(RemoteConfig.camera_brightness)
+            self.fiducial_size_sub = table.getDoubleTopic("fiducial_size").subscribe(RemoteConfig.fiducial_size)
             self.fiducial_layout_sub = table.getDoubleArrayTopic("fiducial_layout").subscribe([])
             self.init_complete = True
 
-            config.remote.camera_id = self.camera_id_sub.get()
-            config.remote.camera_resolution_width = self.camera_resolution_width_sub.get()
-            config.remote.camera_resolution_height = self.camera_resolution_height_sub.get()
-            config.remote.camera_auto_exposure = self.camera_auto_exposure_sub.get()
-            config.remote.camera_exposure = self.camera_exposure_sub.get()
-            config.remote.camera_gain = self.camera_gain_sub.get()
-            config.remote.camera_brightness = self.camera_brightness_sub.get()
+        config.remote.camera_id = self.camera_id_sub.get()
+        config.remote.camera_resolution_width = self.camera_resolution_width_sub.get()
+        config.remote.camera_resolution_height = self.camera_resolution_height_sub.get()
+        config.remote.camera_auto_exposure = self.camera_auto_exposure_sub.get()
+        config.remote.camera_exposure = self.camera_exposure_sub.get()
+        config.remote.camera_gain = self.camera_gain_sub.get()
+        config.remote.camera_brightness = self.camera_brightness_sub.get()
+        config.remote.fiducial_size = self.fiducial_size_sub.get()
+        networkLayout = self.fiducial_layout_sub.get()
+        
+        layout = []
+        for i in range(int(len(networkLayout) / 7)):
+            layout.append([
+                networkLayout[i * 7 + 0], 
+                networkLayout[i * 7 + 1], 
+                networkLayout[i * 7 + 2],
+                networkLayout[i * 7 + 3],
+                networkLayout[i * 7 + 4],
+                networkLayout[i * 7 + 5],
+                networkLayout[i * 7 + 6]
+            ])
 
-            try: 
-                config.remote.fiducial_layout = json.loads(self.fiducial_layout_sub.get())
-            except:
-                config.remote.fiducial_layout = None
-                pass
+        config.remote.fiducial_layout = layout
