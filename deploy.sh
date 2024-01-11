a80331fce5a92a82ecde24527616a69f10aa35df
BenG49 <ben@goldfisher.com>
2024-01-10 20:09:24 -0500

    Fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,14 +3,14 @@
 if ! command -v sshpass &> /dev/null
     # mac install: brew install hudochenkov/sshpass/sshpass
 
-    sshpass -porangepi scp -r . orangepi@orangepi5:~/theia
+    sshpass -porangepi scp -r . orangepi@orangepi5:~/theia 2> /dev/null
     echo "--- RESTARTING THEIA SERVICE ---"
     sshpass -porangepi ssh orangepi@orangepi5 -t "systemctl restart theia"
-    exit 1
+    exit 0
 then
     echo "sshpass could not be found, using ssh"
 
-    scp -r . orangepi@orangepi5:~/theia
+    scp -r . orangepi@orangepi5:~/theia 2> /dev/null
     echo "--- RESTARTING THEIA SERVICE ---"
     ssh orangepi@orangepi5 -t "systemctl restart theia"
 
